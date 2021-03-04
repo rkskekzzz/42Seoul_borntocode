6b871e731569c70efb5ab7d4de85186734c2fad9
Suhyoung Shin <suhshin@c4r9s2.42seoul.kr>
2021-03-04 17:42:39 +0900

    printf

--- Circle-01/ft_printf/ft_print_cal.c
+++ Circle-01/ft_printf/ft_print_cal.c
@@ -12,33 +12,16 @@
 
 #include "ft_printf.h"
 
-int pf_type_handler_s(char* str, t_format *st)
+int pf_max(int a, int b)
 {
-	char* ret;
-	int len;
-
-	len = ft_strlen(str);
-	if (!str)
-		ft_strdup("(null)");
-	if (st->width == -1)
-		return (-1);
-	if (st->dot == 0)
-		st->pre = len;
-	if (ret = strldup(str, pf_max(len, st->width), pf_min(len), st->pre)))
-		return (-1);
-	ft_putstr_fd(ret, 1);
-	return (ft_strlen(ret));
+	if (a >= b)
+		return (a);
+	return (b);
 }
 
-char *ft_strldup(const char *src, int wid, int pre)
+int pf_min(int a, int b)
 {
-	// minus옵션 처리@@!
-	char	*dest;
-
-	if (!(dest = (char *)malloc(wid + 1)))
-		return (NULL);
-	ft_strlcpy(dest, src, pre + 1);
-	ft_memset(dest + pre, 32, wid - pre);
-	dest[wid] = '\0';
-	return ((char *)dest);
+	if (a < b)
+		return (a);
+	return (b);
 }
